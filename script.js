b7cc2513949b6037c0e4b46564fe84eb446151b7
NikiSukiasyan <justplayingcsgo@gmail.com>
2023-02-23 03:37:50 +0400

    finished main page

--- script.js
+++ script.js
@@ -28,3 +28,48 @@ searchButton.disabled = true;
 if (searchInput.value.trim() !== "") {
   searchButton.disabled = false;
 }
+const scrollToTopButton = document.querySelector("#scroll-to-top");
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+const languageButton = document.querySelector(".language-button");
+const languageMenu = document.querySelector(".language-menu");
+
+languageButton.addEventListener("click", () => {
+  languageMenu.style.display =
+    languageMenu.style.display === "block" ? "none" : "block";
+});
+
+languageMenu.addEventListener("click", (event) => {
+  const selectedLanguage = event.target.textContent;
+  const languageInput = document.querySelector(".language-input");
+  languageInput.value = selectedLanguage;
+  languageMenu.style.display = "none";
+});
+
+const lis = document.querySelectorAll(".li-container li");
+
+lis.forEach((li) => {
+  li.addEventListener("mouseover", () => {
+    li.style.fontWeight = "600";
+    li.style.fontFamily = "montserrat";
+    li.style.cursor = "pointer";
+  });
+
+  li.addEventListener("mouseout", () => {
+    li.style.fontWeight = "400";
+    li.style.fontFamily = "montserrat-light";
+    li.style.cursor = "default";
+  });
+});
+function scrollToSection(event) {
+  event.preventDefault();
+  const target = event.target.getAttribute("href");
+  const section = document.querySelector(target);
+  section.scrollIntoView({ behavior: "smooth" });
+}
